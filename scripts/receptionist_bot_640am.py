0fbc2ca489b0908eaede087bede1dc61a18c5cf3
dpssmeditation-cell <dpssmeditation@gmail.com>
2026-08-10 20:16:36 +0700

    Enhance message templates with HTML formatting
    Updated message templates to include HTML formatting for better readability.

--- scripts/receptionist_bot_640am.py
+++ scripts/receptionist_bot_640am.py
@@ -8,45 +8,45 @@ CHAT_ID = os.environ.get("RECEPTIONIST_CHAT_ID", "-1001554726520")
 
 today_date = datetime.datetime.now().strftime("%d.%m.%Y")
 
-MESSAGE_TEMPLATE_1 = f"""1). Absent FT&Khmer Students - Morning Shift ({today_date}). Please write absent/late students in the comment below."""
+MESSAGE_TEMPLATE_1 = f"""1). <b>Absent FT&Khmer Students - Morning Shift ({today_date}).</b> Please write absent/late students in the comment below."""
 
 MESSAGE_TEMPLATE_2 = f"""2).Document Copies 
-Full-time ({today_date}). 
+Full-time <b>({today_date})</b>. 
 Please attach yrs to-be-copied files in the comment below before 03:00pm.  
 Please list down : 
 ⁃ Class time
 ⁃ Number of Copy
 ⁃ Date to use 
 ⁃ level  
-សូមលោកគ្រូអ្នកគ្រូជួយ Edit នៅចុងសន្លឹកទី១ ដាក់ឈ្មោះ កម្រិត ចំនួននិងថ្ងៃផង។ដើម្បីសម្រួលដល់ការច្រឡំក្រដាសគ្នា។
+<b>សូមលោកគ្រូអ្នកគ្រូជួយ Edit នៅចុងសន្លឹកទី១ ដាក់ឈ្មោះ កម្រិត ចំនួននិងថ្ងៃផង។ដើម្បីសម្រួលដល់ការច្រឡំក្រដាសគ្នា។
 Note: សូមលោកគ្រូ-អ្នកគ្រូជួយកែឈ្មោះ file ផង
-Example: Level_Name&Name_(…..)
+Example: Level_Name&Name_(…..)</b>
 Thanks!"""
 
-MESSAGE_TEMPLATE_3 = f"""3).Document Copies 
-Part-Time ({today_date}). 
-Please attach yrs to-be-copied files in the comment below before 03:00pm.  
+MESSAGE_TEMPLATE_3 = f"""3).<b>Document Copies 
+Part-Time ({today_date})</b>. 
+Please attach yrs to-be-copied files in the comment below before <b>03:00pm.</b>  
 Please list down : 
 ⁃ Class time
 ⁃ Number of Copy
 ⁃ Date to use 
 ⁃ level  
-សូមលោកគ្រូអ្នកគ្រូជួយEdit នៅចុងសន្លឹកទី១ ដាក់ឈ្មោះ កម្រិត ចំនួននិងថ្ងៃផង។ដើម្បីសម្រួលដល់ការច្រឡំក្រដាសគ្នា។
+<b>សូមលោកគ្រូអ្នកគ្រូជួយEdit នៅចុងសន្លឹកទី១ ដាក់ឈ្មោះ កម្រិត ចំនួននិងថ្ងៃផង។ដើម្បីសម្រួលដល់ការច្រឡំក្រដាសគ្នា។
 Note: សូមលោកគ្រូ-អ្នកគ្រូជួយកែឈ្មោះ file ផង
-Example: Level_Name&Name_(…..)
+Example: Level_Name&Name_(…..)</b>
 Thanks!"""
 
-MESSAGE_TEMPLATE_4 = f"""4).Document Copies 
-Khmer ({today_date}). 
-Please attach yrs to-be-copied files in the comment below before 03:00pm.  
+MESSAGE_TEMPLATE_4 = f"""4).<b>Document Copies 
+Khmer ({today_date}).</b> 
+Please attach yrs to-be-copied files in the comment below before <b>03:00pm.</b>  
 Please list down : 
 ⁃ Class time
 ⁃ Number of Copy
 ⁃ Date to use 
 ⁃ level  
-សូមលោកគ្រូអ្នកគ្រូជួយEdit នៅចុងសន្លឹកទី១ ដាក់ឈ្មោះ កម្រិត ចំនួននិងថ្ងៃផង។ដើម្បីសម្រួលដល់ការច្រឡំក្រដាសគ្នា។
+<b>សូមលោកគ្រូអ្នកគ្រូជួយEdit នៅចុងសន្លឹកទី១ ដាក់ឈ្មោះ កម្រិត ចំនួននិងថ្ងៃផង។ដើម្បីសម្រួលដល់ការច្រឡំក្រដាសគ្នា។
 Note: សូមលោកគ្រូ-អ្នកគ្រូជួយកែឈ្មោះ file ផង
-Example: Level_Name&Name_(…..)
+Example: Level_Name&Name_(…..)</b>
 Thanks!"""
 
 def send_message(text):
